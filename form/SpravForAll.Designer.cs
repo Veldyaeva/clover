5052d668818f5f10ca8d1d87f2ba21b03cd5c827
maxim <maksim_medvedev_06@mail.ru>
2024-11-18 14:00:52 +0300

    oborud_shv

---
+++ form/SpravForAll.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SewingProduction.form
+{
+    partial class SpravForAll
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gridControlSprav = new DevExpress.XtraGrid.GridControl();
+            this.spravList = new System.Windows.Forms.BindingSource(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.simpleButtonAdd = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlSprav)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spravList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControlSprav
+            // 
+            this.gridControlSprav.DataSource = this.spravList;
+            this.gridControlSprav.Location = new System.Drawing.Point(12, 12);
+            this.gridControlSprav.MainView = this.gridView1;
+            this.gridControlSprav.Name = "gridControlSprav";
+            this.gridControlSprav.Size = new System.Drawing.Size(639, 597);
+            this.gridControlSprav.TabIndex = 0;
+            this.gridControlSprav.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.gridControlSprav.Load += new System.EventHandler(this.gridControlSprav_Load);
+            this.gridControlSprav.Click += new System.EventHandler(this.gridControlSprav_Click);
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControlSprav;
+            this.gridView1.Name = "gridView1";
+            // 
+            // simpleButtonAdd
+            // 
+            this.simpleButtonAdd.Appearance.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.simpleButtonAdd.Appearance.Options.UseFont = true;
+            this.simpleButtonAdd.Location = new System.Drawing.Point(687, 564);
+            this.simpleButtonAdd.Name = "simpleButtonAdd";
+            this.simpleButtonAdd.Size = new System.Drawing.Size(148, 45);
+            this.simpleButtonAdd.TabIndex = 2;
+            this.simpleButtonAdd.Text = "Добавление";
+            this.simpleButtonAdd.Click += new System.EventHandler(this.simpleButtonAdd_Click);
+            // 
+            // SpravForAll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 650);
+            this.Controls.Add(this.simpleButtonAdd);
+            this.Controls.Add(this.gridControlSprav);
+            this.Name = "SpravForAll";
+            this.Text = "Cправочник";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SpravForAll_FormClosing);
+            this.Load += new System.EventHandler(this.SpravForAll_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlSprav)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spravList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControlSprav;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.BindingSource spravList;
+        private DevExpress.XtraEditors.SimpleButton simpleButtonAdd;
+    }
+}
---
+++ form/SpravForAll.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.Utils;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Views.Base;
+using System.Diagnostics;
+using DevExpress.ChartRangeControlClient.Core;
+using DevExpress.XtraGrid.Localization;
+
+namespace SewingProduction.form
+{
+    public partial class SpravForAll : Form
+    {
+        // Оснавная БД:
+        //string connectionString = Properties.Settings.Default.ACEConnectionString;
+        // Для тестов:
+       string connectionString = Properties.Settings.Default.ACEtestConnectionString;
+
+        string queryList;
+        int strForAdd;
+        string tableString;
+        List<string> fieldsQueryListSQL;
+        // Отслеживание изменений в базе данных:
+        private SqlDependency sqlDependency;
+        // Соединение с бд:
+        private SqlConnection connection;
+        //чтобы перейти к нужной строке в таблице:
+        int currentRowIndex = 0;//текущий индекс
+        int topRowIndex = 0;//верхний индекс 
+        bool flagAddDown = false; //если добавили поле в таблицу
+        bool flagStartListening = false; //вкл прослушки
+        public SpravForAll(string tableSQL, string rusNameTableSQL)
+        {
+            GridLocalizer.Active = new RussianGridLocalizer();
+            InitializeComponent();
+            //Таблица:
+            tableString = tableSQL;
+            //Имя формы:
+            this.Text = rusNameTableSQL;
+            //Текст запроса:
+            queryList = $"SELECT * FROM " + tableString;
+            //Иницилизация листа столбцов:
+            fieldsQueryListSQL = new List<string>();
+        }
+
+        private void gridControlSprav_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection connectionLoad = new SqlConnection(connectionString))
+            {
+                //используя подключение отправляем запрос БД:
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(queryList, connectionLoad);
+                //Создаем в памяти таблицу:
+                System.Data.DataTable tableList = new System.Data.DataTable();
+                //Добавляем ответ сервера в таблицу:
+                dataAdapter.Fill(tableList);
+                //Закгрузка в таблицу грида:
+                spravList.DataSource = tableList;
+                // Получаем имена столбцов и добавляем их в список:
+                foreach (System.Data.DataColumn column in tableList.Columns)
+                {
+                    fieldsQueryListSQL.Add(column.ColumnName); // Добавляем имя столбца
+                }
+                // Получаем доступ к GridView
+                GridView gridView = gridControlSprav.MainView as GridView;
+                //Запрет на редактирование
+                gridView.OptionsBehavior.Editable = false;
+                // Выравнивание столбцов
+                gridView.BestFitColumns();
+            }
+            if (!flagStartListening) 
+            {
+                // Запуск отслеживания изменений для соединения с базой данных
+                SqlDependency.Start(connectionString);
+                // Начинаем прослушивание
+                StartListening();
+            }
+        }
+
+        public void StartListening()
+        {
+            try
+            {
+                flagStartListening = true;
+                // Остановка предыдущего прослушивания, если оно было активно:
+                StopListening();
+                // SQL-запрос// Преобразование списка имен столбцов в строку
+                string columns = string.Join(", ", fieldsQueryListSQL);
+                // SQL-запрос
+                string queryOborudList = $"SELECT {columns} FROM dbo.{tableString}";
+                // Создание соединения с базой данных
+                connection = new SqlConnection(connectionString);
+                // Открытие соединения
+                connection.Open();
+                // Создание команды для выполнения SQL-запроса
+                SqlCommand command = new SqlCommand(queryOborudList, connection);
+                // Создание зависимости, чтобы отслеживать изменения
+                sqlDependency = new SqlDependency(command);
+                // Подписка на событие изменения
+                sqlDependency.OnChange += new OnChangeEventHandler(OnDependencyChange);
+                // Выполнение команды
+                command.ExecuteReader();
+
+            }
+            catch (SqlException sqlEx)
+            {
+                Debug.WriteLine($"SQL Error: {sqlEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error starting listener: {ex.Message}");
+            }
+
+        }
+
+        public void StopListening()
+        {
+            // Закрываем подключение
+            if (connection != null)
+            {
+                connection.Close();
+            }
+        }
+        private void OnDependencyChange(object sender, SqlNotificationEventArgs e)
+        {
+            // Строка состояния:
+            Debug.WriteLine($"Notification received: Type={e.Type}, Info={e.Info}, Source={e.Source}");
+            // Проверка есть ли уведомления
+            if (e.Type == SqlNotificationType.Change)
+            {
+                Debug.WriteLine("Data was changed");
+
+                // Обновление UI через Invoke
+                this.Invoke((MethodInvoker)delegate
+                {
+                    GridView gridView = gridControlSprav.MainView as GridView;
+                    // Запомнили положение в таблице:
+                    topRowIndex = gridView.TopRowIndex;
+                    // Обновили таблицу
+                    gridControlSprav_Load(sender, e);
+                    // Возвращаемся к курсору:
+                    gridView.FocusedRowHandle = currentRowIndex;
+                    gridView.TopRowIndex = topRowIndex;
+                    // Если добавлена новая запись, переходим к ней:
+                    if (flagAddDown)
+                    {
+                        gridView.FocusedRowHandle = gridView.RowCount - 1;
+                        gridView.TopRowIndex = gridView.RowCount - 1;
+                        flagAddDown = false;
+                    }
+                });
+
+            }
+            // Возобновляем прослушивание
+            StartListening();
+        }
+        private void simpleButtonAdd_Click(object sender, EventArgs e)
+        {
+            GridView gridView = (GridView)gridControlSprav.MainView;
+            gridView.AddNewRow();
+            //strForAdd = gridView.FocusedRowHandle;
+            //gridView.UpdateCurrentRow();
+            flagAddDown = true;
+        }
+
+        private void gridControlSprav_Click(object sender, EventArgs e)
+        {
+            GridView gridView = gridControlSprav.MainView as GridView;
+            // Сохраняем индекс строки
+            currentRowIndex = gridView.FocusedRowHandle;
+        }
+
+        private void SpravForAll_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopListening();
+        }
+
+        private void SpravForAll_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
