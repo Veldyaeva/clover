6bb627776ab0416159ecb317fcefd1eaa218362b
Bakulina <elenabakulina@cleverv.ru>
2024-12-17 11:08:07 +0300

    add articul

---
+++ form/Articul.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SewingProduction.form
+{
+    partial class Articul
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gridArt = new DevExpress.XtraGrid.GridControl();
+            this.gridControl1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.bsArt = new System.Windows.Forms.BindingSource(this.components);
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridArt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsArt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridArt
+            // 
+            this.gridArt.DataSource = this.bsArt;
+            this.gridArt.Location = new System.Drawing.Point(2, 0);
+            this.gridArt.MainView = this.gridControl1;
+            this.gridArt.Name = "gridArt";
+            this.gridArt.Size = new System.Drawing.Size(614, 562);
+            this.gridArt.TabIndex = 1;
+            this.gridArt.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridControl1});
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn3,
+            this.gridColumn4,
+            this.gridColumn5,
+            this.gridColumn6});
+            this.gridControl1.GridControl = this.gridArt;
+            this.gridControl1.Name = "gridControl1";
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "Код";
+            this.gridColumn1.FieldName = "kod";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "Группа";
+            this.gridColumn2.FieldName = "grup";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "Артикул";
+            this.gridColumn3.FieldName = "articul";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 2;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "Размер";
+            this.gridColumn4.FieldName = "razm";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 3;
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "Модель";
+            this.gridColumn5.FieldName = "mod";
+            this.gridColumn5.Name = "gridColumn5";
+            this.gridColumn5.Visible = true;
+            this.gridColumn5.VisibleIndex = 4;
+            // 
+            // gridColumn6
+            // 
+            this.gridColumn6.Caption = "ТМ";
+            this.gridColumn6.FieldName = "kle_naimen";
+            this.gridColumn6.Name = "gridColumn6";
+            this.gridColumn6.Visible = true;
+            this.gridColumn6.VisibleIndex = 5;
+            // 
+            // Articul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 700);
+            this.Controls.Add(this.gridArt);
+            this.Name = "Articul";
+            this.Text = "Справочник изделий";
+            this.Load += new System.EventHandler(this.Articul_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridArt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsArt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraGrid.GridControl gridArt;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridControl1;
+        private System.Windows.Forms.BindingSource bsArt;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
+    }
+}
---
+++ form/Articul.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SewingProduction.form
+{
+    public partial class Articul : CustomForm
+    {
+        public Articul()
+        {
+            InitializeComponent();
+        }
+
+        private void Articul_Load(object sender, EventArgs e)
+        {
+            string connectionString = Properties.Settings.Default.ACEConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter adapterArt = new SqlDataAdapter();
+                DataTable dtArticul = new DataTable();
+                string queryArt = $"select kod, grup, articul, razm, mod, kle_naimen from dbo.view_art";
+                queryArt += $" order by ikod";
+                //string queryArt = $"exec GetNaklView '{PachKod}' ";
+                SqlCommand commandNaklList = new SqlCommand(queryArt, connection);
+                adapterArt.SelectCommand = commandNaklList;
+                adapterArt.Fill(dtArticul);
+                bsArt.DataSource = dtArticul;
+
+
+            }
+
+        }
+    }
+}
