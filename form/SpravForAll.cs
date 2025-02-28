83497a280558653f538113217a076a89def4e628
pola <hdgcuo415@re146.dev>
2025-02-28 16:48:53 +0300

    321

--- form/SpravForAll.cs
+++ form/SpravForAll.cs
@@ -167,6 +167,7 @@ namespace SewingProduction.form
                     //перенос столбца архив в конец:
                     //gridView.Columns["arhiv"].VisibleIndex = -(gridView.Columns["arhiv"].VisibleIndex - (gridView.Columns.Count - 2));
                 }
+                /* // отключила временно для реальной базы
                 if (!flagStartListening)
                 {
                     // Запуск отслеживания изменений для соединения с базой данных
@@ -174,6 +175,7 @@ namespace SewingProduction.form
                     // Начинаем прослушивание
                     StartListening();
                 }
+                */
             }
             catch (SqlException sqlEx)
             {
