2949d1def55554764b0b95a20e0d5fea83722388
maxim <maksim_medvedev_06@mail.ru>
2025-07-31 11:13:31 +0300

    Upd: св-во "visiblePermission" - как "visible" только главнее

--- Core/Class/CustomControls/CustomTabPage.cs
+++ Core/Class/CustomControls/CustomTabPage.cs
@@ -13,6 +13,8 @@ namespace SewingProduction.Core.Class
     {
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public string ObjectName { get; set; }
+        private bool _visiblePermission = true;
+        private bool _visibleLogic = true;
 
         public void ApplyPermission(UserClass user)
         {
@@ -35,6 +37,37 @@ namespace SewingProduction.Core.Class
             System.Diagnostics.Debug.WriteLine(
                 $"[Доступ TabPage] {ObjectName}: Просмотр={hasRead}, Редактор={hasWrite}, PageVisible={this.PageVisible}"
             );
+        }
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public bool VisiblePermission
+        {
+            get => _visiblePermission;
+            set
+            {
+                _visiblePermission = value;
+                VisibilityHelper.UpdateVisibility(this, _visiblePermission, _visibleLogic);
+            }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public bool VisibleLogic
+        {
+            get => _visibleLogic;
+            set
+            {
+                _visibleLogic = value;
+                VisibilityHelper.UpdateVisibility(this, _visiblePermission, _visibleLogic);
+            }
+        }
+
+        public new bool Visible
+        {
+            get => base.Visible;
+            set
+            {
+                _visibleLogic = value;
+                VisibilityHelper.UpdateVisibility(this, _visiblePermission, _visibleLogic);
+            }
         }
     }
 
