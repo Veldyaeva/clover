6e38658bf20929b144d11bdd3b00a4353f8c6231
maxim <maksim_medvedev_06@mail.ru>
2025-09-19 11:07:15 +0300

    😃

--- Features/Articul/Forms/EditArticul.cs
+++ Features/Articul/Forms/EditArticul.cs
@@ -11,10 +11,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.Pdf.Native;
 using DevExpress.Xpo.DB.Helpers;
+using DevExpress.Xpo.Logger.Transport;
 using DevExpress.XtraEditors;
 using DevExpress.XtraExport.Helpers;
 using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using Org.BouncyCastle.Crypto;
 using SewingProduction.Core.Models;
 using SewingProduction.Features.UserDistribution.Helpers;
 using SewingProduction.Helpers;
@@ -36,9 +38,9 @@ namespace SewingProduction.Features.Articul
             _artNewDataService = new ArtNewDataService(dbHelper);
             ThemeManager.UpdateTheme(this);
             kodSQL = kodArtSQL;
-            customTextBoxKod1.Text = kodSQL;
+            customTextBoxKod.Text = kodSQL;
             visibleSP(false);
-            radioGroup1.SelectedIndex = kodArtSQL == null ? 0 : 2;
+            radioGroup1.SelectedIndex = kodArtSQL == null ? 0 : 2; // новый арт / копия
         }
 
         private void art_new2024_Load(object sender, EventArgs e) => comboAllTableItems();
@@ -59,17 +61,17 @@ namespace SewingProduction.Features.Articul
                     break;
                 // Копия артикула
                 case 2:
-                    if (customTextBoxKod1.Text == "")
+                    if (customTextBoxKod.Text == "")
                     {
                         MessageBox.Show("Введите код артикула!");
                         radioGroup1.SelectedIndex = 0;
-                        customTextBoxKod1.Focus();
+                        customTextBoxKod.Focus();
                     }
                     else
                     {
                         visibleSP(false);
-                        kodSQL = customTextBoxKod1.Text;
-                        copyArt(); 
+                        kodSQL = customTextBoxKod.Text;
+                        copyArt();
                     }
                     break;
             }
@@ -77,8 +79,8 @@ namespace SewingProduction.Features.Articul
 
         private void newArt()
         {
-            customTextBoxKod1.Text = "";
-            customTextBoxKod2.Text = "";
+            customTextBoxKod.Text = "";
+            customTextBoxPo.Text = "";
             customTextBoxArt.Text = "";
             customTextBoxModel.Text = "";
             customTextBoxKodFurn.Text = "";
@@ -126,9 +128,11 @@ namespace SewingProduction.Features.Articul
 
             searchLookUpEditTm1.Properties.DataSource = _artNewDataService.GetViewTovarMarka();
             searchLookUpEditTm1.Properties.DisplayMember = "Наименование";
+            searchLookUpEditTm1.Properties.ValueMember = "kle";
 
             searchLookUpEditTm2.Properties.DataSource = _artNewDataService.GetViewGrupMen();
             searchLookUpEditTm2.Properties.DisplayMember = "Наименование";
+            searchLookUpEditTm2.Properties.ValueMember = "men_id";
 
             searchLookUpEditRazm.Properties.DataSource = _artNewDataService.GetGostSvRazmerAndGostRazmer();
             searchLookUpEditRazm.Properties.DisplayMember = "Размер";
@@ -138,61 +142,160 @@ namespace SewingProduction.Features.Articul
         }
         private void lookUpEditGost_EditValueChanged(object sender, EventArgs e)
         {
+            searchLookUpEditGost.Properties.ValueMember = "Ид";
+            var idGost = searchLookUpEditGost.EditValue?.ToString();
             // группы
-            searchLookUpEditGroup.Properties.DataSource = _artNewDataService.GetGostSvPictAndArticulGrupWhere(searchLookUpEditGost.Text);
+            searchLookUpEditGroup.Properties.DataSource = _artNewDataService.GetGostSvPictAndArticulGrup(idGost);
             searchLookUpEditGroup.Properties.DisplayMember = "Наименование";
             // размеры
-            searchLookUpEditRazm.Properties.DataSource = _artNewDataService.GetGostSvRazmerAndGostRazmerWhere(searchLookUpEditGost.Text);
+            searchLookUpEditRazm.Properties.DataSource = _artNewDataService.GetGostSvRazmerAndGostRazmer(idGost);
             searchLookUpEditRazm.Properties.DisplayMember = "Размер";
         }
         // Копирование артикула
         private void copyArt()
         {
-            var tableList = _artNewDataService.GetSpArticulKod(kodSQL);
+            var tableList = _artNewDataService.GetSpArticulByKod(kodSQL);
             // Загружаем данные:
             if (tableList.Rows.Count > 0)
             {
-                customTextBoxKod1.Text = tableList.Rows[0]["kod"].ToString();
-                customTextBoxArt.Text = tableList.Rows[0]["articul"].ToString();
+                var row = tableList.Rows[0];
 
-                searchLookUpEditRazm.Properties.ValueMember = "Размер";
-                searchLookUpEditRazm.EditValue = tableList.Rows[0]["Размер"].ToString().Trim();
+                customTextBoxKod.Text = row["kod"].ToString();
+
+                searchLookUpEditGost.Properties.ValueMember = "Ид";
+                searchLookUpEditGost.EditValue = row["id_gost"].ToString().Trim();
+
+                searchLookUpEditGroup.Properties.ValueMember = "Наименование";
+                searchLookUpEditGroup.EditValue = row["grup"].ToString().Trim();
 
                 searchLookUpEditTm1.Properties.ValueMember = "kle";
-                searchLookUpEditTm1.Text = tableList.Rows[0]["kle"].ToString().Trim();
+                searchLookUpEditTm1.EditValue = row["kle"].ToString().Trim();
 
                 searchLookUpEditTm2.Properties.ValueMember = "men_id";
-                searchLookUpEditTm2.Text = tableList.Rows[0]["men_id"].ToString().Trim();
+                searchLookUpEditTm2.EditValue = row["men_id"].ToString().Trim();
 
+                customTextBoxArt.Text = row["articul"].ToString();
+
+                searchLookUpEditRazm.Properties.ValueMember = "Размер";
+                searchLookUpEditRazm.EditValue = row["Размер"].ToString().Trim();
+
+                customTextBoxModel.Text = row["mod"].ToString();
             }
-            searchLookUpEditGost.Enabled = false;
-            searchLookUpEditGroup.Enabled = false;
+            //searchLookUpEditGost.Enabled = false;
+            //searchLookUpEditGroup.Enabled = false;
         }
 
         // Кнопка сохранить
-        private async void customOkButton1_Click(object sender, EventArgs e)
+        private void customOkButtonSave_Click(object sender, EventArgs e)
         {
+            var error = checkRule();
+            if (error != null)
+            {
+                MessageBox.Show(error);
+                return;
+            }
             ArticulModel articulModel = new ArticulModel
             {
-                Kod = customTextBoxKod1.Text,
+                Kod = customTextBoxKod.Text,
+                Po = customTextBoxPo.Text,
+                Id_gost = Convert.ToInt32(searchLookUpEditGost.EditValue),
                 Gost = searchLookUpEditGost.Text,
                 Grup = searchLookUpEditGroup.Text,
                 Kle = searchLookUpEditTm1.Text,
                 Articul = customTextBoxArt.Text,
                 Razm = searchLookUpEditRazm.Text,
                 Mod = customTextBoxModel.Text,
-                Komp_name = Environment.MachineName
+                Komp_name = Environment.MachineName,
+                Sql_pr_add = 1,
+                Date_add = DateTime.Now,
+                Ag_id = Convert.ToInt32((searchLookUpEditGroup.Properties.View
+                .GetFocusedRowCellValue("Ag_id") ?? 0)),
+
+                Kod_tnved = searchLookUpEditGroup.Properties.View
+                    .GetFocusedRowCellValue("Ag_tnved")?.ToString(),
+
+                Grupp = Convert.ToInt32(searchLookUpEditTm2.EditValue)
             };
             if (radioGroup1.SelectedIndex == 1) // Новый артикул СП (шнуры,резинка)
             {
-               // articulModel. = ;
+                error = checkRuleRezinka();
+                if (error != null)
+                {
+                    MessageBox.Show(error);
+                    return;
+                }
+                var cordLength = int.TryParse(customTextBoxDlin.Text, out var len) ? len : 0;
+                articulModel.Kod_lv3 = customTextBoxKodFurn.Text;
+                articulModel.Sek_cord = int.TryParse(customTextBoxTimePlet.Text, out var sc) ? sc : 0;
+                articulModel.Norm_cord = int.TryParse(customTextBoxNormP.Text, out var nc) ? nc : 0;
+                articulModel.Norm_t = articulModel.Norm_cord * cordLength;
+                articulModel.Norm_t1 = articulModel.Norm_cord * cordLength;
+                articulModel.Sek_vyaz = articulModel.Sek_cord * cordLength;
+                articulModel.Sek = articulModel.Sek_cord * cordLength;
+                if (!string.IsNullOrEmpty(articulModel.Kod) && articulModel.Kod.Length >= 8)
+                    articulModel.Po = articulModel.Kod.Substring(7, 1);
+
+                articulModel.Tgm_id_n = Convert.ToInt32((searchLookUpEditPrizn.Properties.View
+                .GetFocusedRowCellValue("tcds_id") ?? 0));
             }
-            await _artNewDataService.SaveAsync(articulModel);
+            var props = typeof(ArticulModel).GetProperties();
+            foreach (var p in props)
+            {
+                if (p.GetValue(articulModel) != null)
+                    Debug.WriteLine($"{p.Name} = {p.GetValue(articulModel)}");
+            }
+            //await _artNewDataService.SaveAsync(articulModel);
+
             this.DialogResult = DialogResult.OK;
             this.Close();
+
         }
-        // Кнопка отмена
-        private void customCancelButton1_Click(object sender, EventArgs e)
+        string checkRule()
+        {
+            int kod = _artNewDataService.GetArticulByKod(customTextBoxKod.Text);
+            if (kod > 0)
+            {
+                return ("Такой код артикула уже есть!");
+            }
+            if (string.IsNullOrWhiteSpace(customTextBoxKod.Text))
+            {
+                return ("Укажите код!");
+            }
+            if (string.IsNullOrWhiteSpace(searchLookUpEditGost.Text) || searchLookUpEditGost.EditValue == null)
+            {
+                return ("Выберите гост!");
+            }
+            if (string.IsNullOrWhiteSpace(searchLookUpEditGroup.Text))
+            {
+                return ("Выберите группу!");
+            }
+            if (string.IsNullOrWhiteSpace(customTextBoxArt.Text))
+            {
+                return ("Введите артикул!");
+            }
+            return null;
+        }
+        string checkRuleRezinka()
+        {
+            if (string.IsNullOrWhiteSpace(customTextBoxKodFurn.Text))
+            {
+                return ("Укажите код материала!");
+            }
+            if (!int.TryParse(customTextBoxTimePlet.Text, out var sekCord) || sekCord == 0)
+            {
+                return ("Укажите секунды плетения на 1 метр!");
+            }
+            if (!int.TryParse(customTextBoxNormP.Text, out var normCord) || normCord == 0)
+            {
+                return ("Укажите норму пряжи на 1 метр!");
+            }
+            if (searchLookUpEditPrizn.EditValue == null)
+            {
+                return ("Укажите признак шнура!");
+            }
+            return null;
+        }
+        private void customButtonCancel_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Cancel;
             this.Close();
@@ -217,6 +320,7 @@ namespace SewingProduction.Features.Articul
                 }
             }
         }
+
     }
-    
+
 }
