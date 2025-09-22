eeb300f95c3cfbb3b0bf2ba462d9ee399848f0ad
maxim <maksim_medvedev_06@mail.ru>
2025-09-22 10:05:36 +0300

    Fix: EditArticul; Upd: + CustomSearchLookUpEdit;

--- Core/Class/CustomControlsThemeable.cs
+++ Core/Class/CustomControlsThemeable.cs
@@ -399,67 +399,7 @@ namespace SewingProduction
             }
         }
     }
-    public class CustomCheckedListBox : CheckedListBox, IThemeable, IThemeableControl
-    {
-        public string ObjectName { get; set; }
-        private bool _visiblePermission = true;
-        private bool _visibleLogic = true;
-        public CustomCheckedListBox()
-        {
-            ApplyTheme();
-            ThemeManager.ThemeChanged += OnThemeChanged;
-        }
-        public void ApplyTheme()
-        {
-            BackColor = ThemeManager.ActiveTheme.TextBoxBackground;
-            ForeColor = ThemeManager.ActiveTheme.TextBoxText;
-            Font = ThemeManager.SharedSettings.DefaultFont;
-        }
-        private void OnThemeChanged() => ApplyTheme();
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                ThemeManager.ThemeChanged -= OnThemeChanged;
-            }
-            base.Dispose(disposing);
-        }
-        public void ApplyPermission(UserClass user)
-        {
-            PermissionHelper.ApplyTo(this, ObjectName, user);
-        }
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public bool VisiblePermission
-        {
-            get => _visiblePermission;
-            set
-            {
-                _visiblePermission = value;
-                VisibilityHelper.UpdateVisibility(this, _visiblePermission, _visibleLogic);
-            }
-        }
-
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public bool VisibleLogic
-        {
-            get => _visibleLogic;
-            set
-            {
-                _visibleLogic = value;
-                VisibilityHelper.UpdateVisibility(this, _visiblePermission, _visibleLogic);
-            }
-        }
-
-        public new bool Visible
-        {
-            get => base.Visible;
-            set
-            {
-                _visibleLogic = value;
-                VisibilityHelper.UpdateVisibility(this, _visiblePermission, _visibleLogic);
-            }
-        }
-    }
+    
     public class CustomTextBoxEx : DevExpress.XtraEditors.TextEdit, IThemeable, IThemeableControl
     {
         public string ObjectName { get; set; }
