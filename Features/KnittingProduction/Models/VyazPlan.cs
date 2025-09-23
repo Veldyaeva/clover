16a42e64cb32858b9d77ae6654e45980a9401ae8
NadezhdaPentegova <progr4@acewear.ru>
2025-09-23 13:38:44 +0300

    исправила ошибку с выбором заданий для назначения В/М

--- Features/KnittingProduction/Models/VyazPlan.cs
+++ Features/KnittingProduction/Models/VyazPlan.cs
@@ -55,7 +55,7 @@ namespace SewingProduction.Features.KnittingProduction.Models
         [NotMapped]
         public string PictPath { get; set; }
         [NotMapped]
-        public int SyncSelection { get; set; } = 0;
+        public bool SyncSelection { get; set; } = false;
         [NotMapped]
         public bool IsModified { get; set; } = false;
         [NotMapped]
