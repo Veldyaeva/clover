3d1bbdfacd5e959c501435600d0a753a2d401d42
Pola <opopova_1985@mail.ru>
2025-10-16 10:28:11 +0300

    feat: раскомментила инсерт, добавила себя в рассылку

--- Features/TeamWork/Services/ArtNormService.cs
+++ Features/TeamWork/Services/ArtNormService.cs
@@ -758,7 +758,7 @@ WHERE nr.annId = @annId";
     }
     public interface IJabberSender
     {
-        Task SendToBrigsAsync(IEnumerable<int> brigIds, string message, int idType = 14);
+        Task SendToBrigsAsync(IEnumerable<int> brigIds, string message, int idType = 14, int tester = 63);
     }
 
     public sealed class JabberSender : IJabberSender
@@ -769,7 +769,7 @@ WHERE nr.annId = @annId";
         private readonly DbService _dbService;
         public JabberSender(DatabaseHelper dbHelper)
        => _dbHelper = dbHelper ?? throw new ArgumentNullException(nameof(dbHelper));
-        public async Task SendToBrigsAsync(IEnumerable<int> brigIds, string message, int idType = 14)
+        public async Task SendToBrigsAsync(IEnumerable<int> brigIds, string message, int idType = 14, int tester = 63)
         {
             var ids = brigIds?
                 .Where(id => id > 0)
@@ -783,15 +783,15 @@ WHERE nr.annId = @annId";
 
 SELECT DISTINCT @msg, v.icq
 FROM [view_sprav_men] v
-WHERE v.id_type = @idType
-  AND v.id_brig IN @brigIds
+WHERE (v.id_type = @idType AND v.id_brig IN @brigIds) 
+    OR (v.id_type = @tester)
   AND v.icq IS NOT NULL;";
             await _logger.LogEventAsync(sql);
             Debug.WriteLine(sql);
             try
             {
                 using var connection = _dbHelper.GetConnection();
-                await connection.ExecuteAsync(sql, new { msg = message, idType, brigIds = ids });
+                await connection.ExecuteAsync(sql, new { msg = message, idType, brigIds = ids, tester });
                 await _logger.LogEventAsync(
                     $"Jabber: '{message}' отправлено в {ids.Length} бригад(ы). {ids}",
                     "JabberSender");
