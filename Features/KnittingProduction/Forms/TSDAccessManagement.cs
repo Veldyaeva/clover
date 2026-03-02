00e7e5a019b6bc75d627c77f4ceac86ac63e1bd0
NadezhdaPentegova <progr4@acewear.ru>
2026-03-02 09:21:01 +0300

    1

--- Features/KnittingProduction/Forms/TSDAccessManagement.cs
+++ Features/KnittingProduction/Forms/TSDAccessManagement.cs
@@ -1,4 +1,10 @@
-﻿using SewingProduction.Features.UserDistribution.Helpers;
+﻿using SewingProduction.Core.helpers;
+using SewingProduction.Core.services;
+using SewingProduction.Features.KnittingProduction.Models;
+using SewingProduction.Features.KnittingProduction.Services;
+using SewingProduction.Features.UserDistribution.Helpers;
+using SewingProduction.Helpers;
+using SewingProduction.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,9 +19,27 @@ namespace SewingProduction.Features.KnittingProduction.Forms
 {
     public partial class TSDAccessManagement : CustomForm
     {
+        private static DatabaseHelper _dbHelper;
+        private static DbService _dbService;
+        private static BulkHelper _bulkHelper;
+        private static GridHelper _gridHelper;
+        private readonly ILogger _logger = new FileLogger();
+        //private readonly VyazService _vyazService;
+        private BindingSource _smenZadanyVyazBindingSource;
         public TSDAccessManagement(UserClass User) : base(User)
         {
             InitializeComponent();
+            _dbHelper = new DatabaseHelper("ace");
+            _dbService = new DbService(_dbHelper);
+            _bulkHelper = new BulkHelper();
+            _gridHelper = new GridHelper();
+
+            //_vyazService = new VyazService(_dbHelper);
+
+            _smenZadanyVyazBindingSource = new BindingSource
+            {
+                DataSource = new BindingList<SmenZadanyVyaz>()
+            };
         }
     }
 }
